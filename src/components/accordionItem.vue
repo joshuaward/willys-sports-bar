59aae8802de21a2ff37776740135a4c0ec15d144
joshuaward <joshua.ward@me.com>
2019-05-10 15:43:41 -0600

    remove console statement

--- src/components/accordionItem.vue
+++ src/components/accordionItem.vue
@@ -2,11 +2,12 @@
 	.accordion-item(@click="open = !open", :class="{ 'is-open': open }")
 			.accordion-trigger {{ item.title }}
 			transition(
-				name="slide", 
-				v-on:enter="enter", 
-				v-on:after-enter="afterEnter", 
+				name="slide",
+				v-on:enter="enter",
+				v-on:after-enter="afterEnter",
 				v-on:leave="leave",
-				v-bind:css="false", duration="250")
+				v-bind:css="false",
+				duration="250")
 				.accordion-content(v-if="open")
 					p {{ item.text }}
 </template>
@@ -29,7 +30,6 @@
 				el.parentElement.insertAdjacentElement('beforeend', clone)
 				setTimeout(()=>{
 					this.height = clone.offsetHeight
-					console.log(this.height)
 					el.style.height = `${this.height}px`
 					el.parentElement.removeChild(clone)
 				})
