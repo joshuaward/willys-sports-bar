be0f3275ecb5c1c1bbd8301de04f36a8b1e29748
joshuaward <joshua.ward@me.com>
2019-07-05 17:40:43 -0600

    remove accordion animations and add backup google font

--- src/components/accordionItem.vue
+++ src/components/accordionItem.vue
@@ -1,15 +1,8 @@
 <template lang="pug">
 	.accordion-item(@click="open = !open", :class="{ 'is-open': open }")
 		.accordion-trigger {{ item.title }}
-		transition(
-			name="slide",
-			v-on:enter="enter",
-			v-on:after-enter="afterEnter",
-			v-on:leave="leave",
-			v-bind:css="false",
-			duration="250")
-			.accordion-content(v-if="open")
-				p {{ item.text }}
+		.accordion-content(v-if="open")
+			p {{ item.text }}
 </template>
 
 <script>
@@ -25,24 +18,24 @@
 			'item'
 		],
 		methods: {
-			enter(el, done) {
-				let clone = el.cloneNode(true)
-				clone.style.height = "auto"
-				el.parentElement.insertAdjacentElement('beforeend', clone)
-				setTimeout(()=>{
-					this.height = clone.offsetHeight
-					el.style.height = `${this.height}px`
-					el.parentElement.removeChild(clone)
-				})
-				done()
-			},
-			leave(el, done) {
-				el.style.height = `${el.offsetHeight}px`
-				setTimeout(()=> {
-					el.style.height = '0px'
-				})
-				done()
-			}
+		// 	enter(el, done) {
+		// 		let clone = el.cloneNode(true)
+		// 		clone.style.height = "auto"
+		// 		el.parentElement.insertAdjacentElement('beforeend', clone)
+		// 		setTimeout(()=>{
+		// 			this.height = clone.offsetHeight
+		// 			el.style.height = `${this.height}px`
+		// 			el.parentElement.removeChild(clone)
+		// 		})
+		// 		done()
+		// 	},
+		// 	leave(el, done) {
+		// 		el.style.height = `${el.offsetHeight}px`
+		// 		setTimeout(()=> {
+		// 			el.style.height = '0px'
+		// 		})
+		// 		done()
+		// 	}
 		}
 	}
 </script>
