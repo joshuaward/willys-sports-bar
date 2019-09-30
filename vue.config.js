b109608521bd392475492fffb8f03a43476176cd
joshuaward <joshua.ward@me.com>
2019-09-30 06:37:03 -0600

    run build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // UNCOMMENT TO CREATE PRODUCTION BUILD
-// module.exports = {
-// 	publicPath: process.env.NODE_ENV === 'production'
-// 	  ? '/'
-// 	  : '/dev/'
-// }
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+	  ? '/'
+	  : '/dev/'
+}
