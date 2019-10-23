53cc8a7386eb4f9a173abd630aa8e001d7477d2b
joshuaward <joshua.ward@me.com>
2019-10-23 16:46:16 -0600

    finish styling zip dish link

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -6,6 +6,8 @@
 					h1.title {{ title }}
 				.cell
 					.hours
+						a.zipDish(href="https://wtn.zipdishdelivery.com/order/rv/61" target="_blank")
+							img.zipDish-logo(src="@/assets/img/zip-dish-logo.png" alt="Zip Dish Logo")
 						h3 Hours
 						ul.hours-list
 							li Sunday: 10:00 am - 12:00 am
@@ -15,8 +17,7 @@
 							li Saturday: 10:00 am - 2:00 am
 						.hours-happy
 							h4 Happy Hour Mon - Fri 4:00pm - 7:00pm
-		a.zipDish(href="https://wtn.zipdishdelivery.com/order/rv/61" target="_blank")
-			img.zipDish-logo(src="@/assets/img/zip-dish-logo.png" alt="Zip Dish Logo")
+			
 		.plax
 </template>
 
